13e75121da0d3c381738aeaf154f2d7d1591f530
poutingemoji <9awu680@gmail.com>
2020-07-26 14:25:15 -0700

    deploy to heroku

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,16 @@
+
+
 $(document).ready(function(){
-    $('a').attr({
-        target: "_blank",
-        rel: "nofollow"
-    })
+    $('a').each(function() {
+        if( location.hostname === this.hostname || !this.hostname.length ) {
+            $(this).addClass('local');
+        } else {
+            $(this).addClass('external');
+            $(this).attr({
+                target: "_blank",
+                rel: "nofollow"
+              })
+        }
+      });
   });
   console.log('yes')
