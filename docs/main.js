4f9db79fa10827102e318e64917825c93f2d1192
poutingemoji <9awu680@gmail.com>
2020-07-26 03:53:38 -0700

    deploy to heroku

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,7 @@
-window.onload = function(){
-    var anchors = document.getElementById('link_other').getElementsByTagName('a');
-    for (var i=0; i<anchors.length; i++){
-      anchors[i].setAttribute('target', '_blank');
-    }
-  }
+$(document).ready(function(){
+    $('a').attr({
+        target: "_blank",
+        rel: "nofollow"
+    })
+  });
+  console.log('yes')
